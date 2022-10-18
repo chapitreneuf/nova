331678139d1fbaef420d838a7744e5927883f975
Thomas Brouard <contact@brrd.fr>
2022-10-18 14:25:49 +0200

    check-contrast.js : add warning message on top

--- public/js/check-contrast.js
+++ public/js/check-contrast.js
@@ -10,11 +10,24 @@ $(function () {
     return errors;
   }
 
+  function rgbToHex(rgb) {
+    function valueToHex(value) {
+      var hex = Math.floor(value).toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    var values = rgb.match(/\d+/g);
+    if (values === null) return rgb;
+    return "#" + values.map(valueToHex).join("");
+  }
+
   function mergeErrors(errors) {
     return errors.reduce(function (res, e) {
       $el = $(e.name[0]);
-      var color = $el.css("color");
-      var bg = contrast.getBackground($el);
+      var rgbColor = $el.css("color");
+      var rgbBg = contrast.getBackground($el);
+      var color = rgbToHex(rgbColor);
+      var bg = rgbToHex(rgbBg);
       if (!res[color]) {
         res[color] = [];
       }
@@ -25,10 +38,28 @@ $(function () {
     }, {});
   }
 
+  function displayNotifications(notifications) {
+    if (!notifications || notifications.length === 0) return;
+    $(".contrast-warning").remove();
+    var content = "<p>&#9888;&#65039; <b>Les couleurs utilisées sur cette page ne respectent pas <a href='https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode-rgaa/criteres/#topic3' target='_blank'>les normes d'accessibilité du référentiel RGAA 4.1</a>.</b></p><p>Des contrastes trop faibles entre les couleurs peuvent gêner la navigation de certains visiteurs. Il est conseillé de remplacer les couleurs suivantes :</p><ul></ul>";
+    $container = $("<div class='contrast-warning lodeluser-info'>" + content + "</div>");
+    $ul = $container.find("ul");
+
+    Object.keys(notifications).forEach(function(color) {
+      var bgColors = notifications[color].join(", ");
+      var msg = "La couleur " + color + " n'est pas lisible sur les arrière-plans suivants : " + bgColors + ".";
+      $("<li>" + msg + "</li>").appendTo($ul);
+    });
+
+    $container.insertBefore("#skip-links");
+  }
+
   function waitAndRun() {
     var errors = getContrastErrors();
-    errors = mergeErrors(errors);
-    console.log(errors);
+    if (!errors || errors.length === 0) return;
+    console.info("Les problèmes de contraste suivants ont été détectés :", errors);
+    var notifications = mergeErrors(errors);
+    displayNotifications(notifications);
   }
 
   if (window.devMode === true) {
